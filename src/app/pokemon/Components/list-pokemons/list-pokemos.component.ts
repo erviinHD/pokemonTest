39fde8450b31fbfeee25596bd544668ec4acbb9d
ervin cabascango <ervin.cabascango.s@gmail.com>
2022-09-07 01:38:48 -0500

    Feat: edit pokemon

--- src/app/pokemon/Components/list-pokemons/list-pokemos.component.ts
+++ src/app/pokemon/Components/list-pokemons/list-pokemos.component.ts
@@ -16,8 +16,11 @@ export class ListPokemosComponent implements OnInit {
   pokemons: Pokemon[] = []
   subscriptions: Subscription[] = [];
 
+  pokemon: Pokemon = { attack: 0, defense: 0, hp: 0, idAuthor: 1, image: '', name: '', type: '', id: 1 }
+
   pokemonNameSearch: string = ''
   isOpen: boolean = false;
+  isEdit: boolean = false;
 
   formPokemon: FormGroup;
 
@@ -43,6 +46,16 @@ export class ListPokemosComponent implements OnInit {
     });
   }
 
+  setDataForm() {
+    this.formPokemon.controls['name'].setValue(this.pokemon.name);
+    this.formPokemon.controls['image'].setValue(this.pokemon.image);
+    this.formPokemon.controls['attack'].setValue(this.pokemon.attack);
+    this.formPokemon.controls['defense'].setValue(this.pokemon.defense);
+    this.formPokemon.controls['hp'].setValue(100);
+    this.formPokemon.controls['type'].setValue("normal");
+    this.formPokemon.controls['idAuthor'].setValue(1);
+  }
+
   async loadData() {
     this.pokemons = await this.getPokemos();
   }
@@ -56,6 +69,23 @@ export class ListPokemosComponent implements OnInit {
         });
       this.subscriptions.push(subPokemonGet);
     })
+  }
+
+  getPokemonById(idPokemon: string) {
+    this.isEdit = true;
+    this.isOpen = true;
+    this.formPokemon.reset()
+
+    const subPokemonGetById = this._pokemon
+      .getPokemonById(idPokemon)
+      .subscribe({
+        next: (resp) => {
+          this.pokemon = resp;
+        }, complete: () => {
+          this.setDataForm()
+        }
+      });
+    this.subscriptions.push(subPokemonGetById);
   }
 
   deletePokemon(pokemon: Pokemon) {
@@ -86,8 +116,25 @@ export class ListPokemosComponent implements OnInit {
     }
   }
 
+  updatePokemon() {
+    if (this.formPokemon.valid) {
+      return new Promise((resolve, rejects) => {
+        const subPokemonPost = this._pokemon.putPokemon(this.pokemon.id.toString(), this.formPokemon.value)
+          .subscribe((resp) => {
+            resolve(resp)
+            this.loadData()
+            this.clearForm()
+          })
+        this.subscriptions.push(subPokemonPost)
+      })
+    }
+  }
+
+
+  // Forms validators
   clearForm() {
     this.isOpen = !this.isOpen
+    this.isEdit = false
     this.formPokemon.reset()
   }
 
--- src/app/pokemon/Services/pokemon.service.ts
+++ src/app/pokemon/Services/pokemon.service.ts
@@ -13,15 +13,25 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
+  deletePokemon(idPokemon: string): Observable<any> {
+    return this.http.delete<any>(`${this.URL_API}/${idPokemon}`);
+  }
+
   getPokemons(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>(`${this.URL_API}/?idAuthor=1`);
+  }
+
+  getPokemonById(idPokemon: string): Observable<Pokemon> {
+    return this.http.get<Pokemon>(`${this.URL_API}/${idPokemon}`);
   }
 
   postPokemon(pokemon: Pokemon): Observable<Pokemon> {
     return this.http.post<Pokemon>(`${this.URL_API}/?idAuthor=1`, pokemon);
   }
 
-  deletePokemon(idPokemon: string): Observable<any> {
-    return this.http.delete<any>(`${this.URL_API}/${idPokemon}`);
+  putPokemon(idPokemon: string, pokemon: Pokemon): Observable<Pokemon> {
+    return this.http.put<Pokemon>(`${this.URL_API}/${idPokemon}`, pokemon);
   }
+
+
 }
